3aed3c5501d6be8444201029c7bb138cc9d25010
bodwood <bodiewood@hotmail.com>
2022-10-18 21:39:03 -0700

    display which player won the dice roll

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,11 @@ let diceImage2 = 'img/dice' + dice2 + '.png';
 
 document.querySelectorAll('img')[0].setAttribute('src', diceImage);
 document.querySelectorAll('img')[1].setAttribute('src', diceImage2);
+
+if (dice1 > dice2) {
+  document.querySelector('h1').innerHTML = '🚩Player 1 Wins!';
+} else if (dice1 < dice2) {
+  document.querySelector('h1').innerHTML = 'Player 2 Wins!🚩';
+} else {
+  document.querySelector('h1').innerHTML = 'Draw';
+}
